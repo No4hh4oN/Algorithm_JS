ca671393a84ba558e39f1e4cc0294e8a32a9add4
No4h <155427247+No4hh4oN@users.noreply.github.com>
2026-01-27 16:19:48 +0900

    [level 2] Title: 전화번호 목록, Time: 133.37 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,11 @@
+// 프로그래머스 전화번호 목록
+function solution(phone_book) {
+    phone_book.sort();
+
+    for (let i = 0; i < phone_book.length - 1; i++) {
+        if (phone_book[i + 1].startsWith(phone_book[i])) {
+            return false;
+        }
+    }
+    return true;
+}
